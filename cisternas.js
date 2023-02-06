954f30055283867bd1be74f2754145b467e9e6b2
Arath García <arathgg143@gmail.com>
2023-02-06 04:09:08 -0600

    solved problem with deploying

--- cisternas.js
+++ cisternas.js
@@ -1,30 +1,48 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCmeaaw2VUT49d7Abl4OYfXpQfQlqk8Oqc",
-    authDomain: "fir-tam-arduino.firebaseapp.com",
-    databaseURL: "https://fir-tam-arduino-default-rtdb.firebaseio.com",
-    projectId: "fir-tam-arduino",
-    storageBucket: "fir-tam-arduino.appspot.com",
-    messagingSenderId: "578061028938",
-    appId: "1:578061028938:web:ff62acaa1f9504019ae0b3"
-  };
- 
-// initialize firebase
-firebase.initializeApp(firebaseConfig);
-
-// reference your database
-
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
+  apiKey: "AIzaSyCmeaaw2VUT49d7Abl4OYfXpQfQlqk8Oqc",
+  authDomain: "fir-tam-arduino.firebaseapp.com",
+  databaseURL: "https://fir-tam-arduino-default-rtdb.firebaseio.com",
+  projectId: "fir-tam-arduino",
+  storageBucket: "fir-tam-arduino.appspot.com",
+  messagingSenderId: "578061028938",
+  appId: "1:578061028938:web:ff62acaa1f9504019ae0b3"
 };
 
-var firebaseRef = firebase.database().ref("temperatureReal");
-firebaseRef.on("value", function(snapshot){
-    var data = snapshot.val();
-    snapshot.forEach(function(element){
-        document.querySelector('#root').innerHTML =`
-            <div>${element.val()}</div>
-        `
-        //console.log(data[i]);
-    });
-})
+firebase.initializeApp(firebaseConfig);
 
+var firebaseRef2 = firebase.database().ref("SalaA");
+function createCircle(elementName) {
+  var containerId = "#container2_" + elementName;
+  var bar = new ProgressBar.Circle(containerId, {
+      color: "blue",
+      strokeWidth: 5,
+      trailWidth: 5,
+      duration: 1500,
+      text: {
+          value: "0"
+      },
+      step: function (state, bar) {
+          bar.setText((bar.value() * 100).toFixed(0));
+      }
+  });
+
+  firebaseRef2.child(elementName).on("value", function (snapshot) {
+      bar.animate(snapshot.val() / 100);
+  });
+}
+
+createCircle("cisterna1");
+createCircle("cisterna2");
+createCircle("cisterna3");
+
+
+// Include the navbar
+function loadNavbar() {
+  fetch("navbar.html")
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector("#navbarContainer").innerHTML = data;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", loadNavbar);
