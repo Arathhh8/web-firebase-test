e886fdbe213ef1fa6a01002c9a18158c11473317
Arath García <arathgg143@gmail.com>
2023-02-25 21:45:10 -0600

    calculation of m3 added in js

--- cisternas.js
+++ cisternas.js
@@ -10,27 +10,30 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const fill = (elementId, firebasePath) => {
+const fill = (elementId, firebasePath,cistern_long, cistern_width) => {
   firebase.database().ref(firebasePath).on("value", function(snapshot) {
     const data = snapshot.val();
-     valor_map = Math.round(map(data, 0, 230, 0, 100));
+     valor_map = Math.round(map(data, 0, 230, 100, 0));
     const element = document.getElementById(elementId);
+    const cistern_height = 2.3;
     element.style.height = `${valor_map}%`;
-    element.innerHTML = `<div class="fill-value"><h4>${valor_map}%</h4><h5>${Math.round((data/100) * 3.3 * 3.75)} m3</h5></div>`; //muestra valor mapeado en el contenedor de la cisterna
+  
+    element.innerHTML = `<div class="fill-value"><h4>${valor_map}%</h4><h5>${((cistern_height - data/100) * cistern_long * cistern_width).toFixed(2)} m<sup>3</sup></h5></div>`; //muestra valor mapeado en el contenedor de la cisterna
     
-    if (data < 20) {
-      element.style.backgroundColor = "red";
+    if (valor_map > 20) {
+      element.style.backgroundColor = "rgb(53, 172, 228)";
     } 
     else{
-      element.style.backgroundColor = "rgb(53, 172, 228)";
+      element.style.backgroundColor = "red";
     }
 
   });
 }
 
-fill("fill1", "SalaA/cisterna1");
-fill("fill2", "SalaA/cisterna2");
-fill("fill3", "SalaA/cisterna3");
+//    CSS, Ruta-Firebase ,cistern_long, cistern_width 
+fill("fill1", "SalaA/cisterna1",3.3, 3.75);
+fill("fill2", "SalaA/cisterna2",8.8, 4.4);
+fill("fill3", "SalaA/cisterna3",3.8, 5);
 
 
 // Include the navbar
