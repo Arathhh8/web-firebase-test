0db2c49022df08ab52ed87da22e544f7a7bbc7aa
Arath Grarcía <arathgg143@gmail.com>
2023-03-28 20:40:15 -0600

    problem with incorrect sensor readings fixed

--- src/js/cisternsA.js
+++ src/js/cisternsA.js
@@ -9,20 +9,33 @@ function loadCisternsA() {
         });
 }
 
+let lastValidValue = null;
+
 export const fill = (elementId, firebasePath,cistern_long, cistern_width) => {
     firebase.database().ref(firebasePath).on("value", function(snapshot) {
       const data = snapshot.val();
+      if (data === 0) { //Corrección a error del sensor, en caso de que envíe un 0, el dato se mantendrá como el anterior
+        if (lastValidValue !== null) {
+          data = lastValidValue;
+        } else {
+          const element = document.getElementById(elementId);
+          element.innerHTML = `<div class="fill-value"><h4>No disponible</h4><h5></h5></div>`;
+          return;
+        }
+      } else {
+        lastValidValue = data;
+      }
+      
       const valor_map = Math.round(map(data, 0, 230, 100, 0));
       const element = document.getElementById(elementId);
       const cistern_height = 2.3;
       element.style.height = `${valor_map}%`;
     
-      element.innerHTML = `<div class="fill-value"><h4>${valor_map}%</h4><h5>${((cistern_height - data/100) * cistern_long * cistern_width).toFixed(2)} m<sup>3</sup></h5></div>`; //muestra valor mapeado en el contenedor de la cisterna
+      element.innerHTML = `<div class="fill-value"><h4>${valor_map}%</h4><h5>${((cistern_height - data/100) * cistern_long * cistern_width).toFixed(2)} m<sup>3</sup></h5></div>`;
       
       if (valor_map > 20) {
         element.style.backgroundColor = "rgb(53, 172, 228)";
-      } 
-      else{
+      } else {
         element.style.backgroundColor = "red";
       }
   
@@ -32,3 +45,5 @@ export const fill = (elementId, firebasePath,cistern_long, cistern_width) => {
   function map(valor, rango_original_min, rango_original_max, rango_destino_min, rango_destino_max) {
     return ((valor - rango_original_min) / (rango_original_max - rango_original_min)) * (rango_destino_max - rango_destino_min) + rango_destino_min;
   }
+
+
