e23d5ad58121c562d36ea6f090c86e93a32be40f
Arathhh8 <arath@192.168.1.6>
2023-06-22 22:29:42 -0600

    welcomeMessage added

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -8,5 +8,15 @@ function loadNavbar() {
     });
 }
 
+// Include the welcome message
+document.addEventListener("DOMContentLoaded", loadWelcomeMessage);
+function loadWelcomeMessage() {
+  fetch("/src/html/welcomeMessage.html")
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector("#welcomeMessage").innerHTML = data;
+    });
+}
+
 
 
