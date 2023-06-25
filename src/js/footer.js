63b2a531e7cc5aed343540367c6c498c0333c2e2
Arath Grarcía <arathgg143@gmail.com>
2023-06-24 19:43:40 -0600

    second deploy (cistern A and cistern C)

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,7 +1,7 @@
 // Include the footer
 document.addEventListener("DOMContentLoaded", loadFooter);
 function loadFooter() {
-  fetch("/src/html/footer.html")
+  fetch("./html/footer.html")
     .then(response => response.text())
     .then(data => {
       document.querySelector("#footerContainer").innerHTML = data;
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,7 +1,7 @@
 // Include the navbar
 document.addEventListener("DOMContentLoaded", loadNavbar);
 function loadNavbar() {
-  fetch("/src/html/navbar.html")
+  fetch("./html/navbar.html")
     .then(response => response.text())
     .then(data => {
       document.querySelector("#navbarContainer").innerHTML = data;
@@ -11,7 +11,7 @@ function loadNavbar() {
 // Include the welcome message
 document.addEventListener("DOMContentLoaded", loadWelcomeMessage);
 function loadWelcomeMessage() {
-  fetch("/src/html/welcomeMessage.html")
+  fetch("./html/welcomeMessage.html")
     .then(response => response.text())
     .then(data => {
       document.querySelector("#welcomeMessage").innerHTML = data;
